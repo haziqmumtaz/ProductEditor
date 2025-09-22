a5496e710b76140fbbee02edfd50dbf4c3dc1348
Haziq Mumtaz <haziq@rayn.group>
2025-09-22 23:43:29 +0500

    update preview host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,11 @@ import viteSvgIcons from "vite-svg-loader";
 export default defineConfig({
   plugins: [vue(), tailwindcss(), viteSvgIcons()],
   preview: {
-    allowedHosts: ["*"],
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "0.0.0.0",
+      "codaproducteditorfrontend-production.up.railway.app",
+    ],
   },
 });
